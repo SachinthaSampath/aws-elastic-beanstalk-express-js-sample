0037f4fd54b8821b4c10cbf827a83e1a1133e091
Sachintha Sampath <sachinsampath42@gmail.com>
2023-06-16 11:56:55 +0530

    chore: Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello new world!'));
+app.get('/', (req, res) => res.send('Hellow! AWS CI/CD is working!!! :)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
